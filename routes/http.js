c91167444e1b4131368c86bfbd94bd4aced06ff3
Victor Tesoura Júnior <txsoura@yahoo.com>
2022-06-08 22:45:10 -0300

    config: create project

---
+++ routes/http.js
@@ -0,0 +1,18 @@
+import { Route } from "@athenna/http";
+
+/*
+|--------------------------------------------------------------------------
+| Http Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register http routes for your application. These
+| routes are loaded by the HttpRouteProvider.
+|
+*/
+
+Route.group(() => {
+  Route.get("/", "WelcomeController.show");
+
+  Route.group(() => {
+  }).prefix("/api/v1");
+});
